a70ce513414079b569e0ec113a0d7efd1be7360f
LeeNgari <leengari76@gmail.com>
2026-03-13 18:03:50 +0300

    supabase works and auth configured

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+const supabaseUrl = "https://netvvdpfyyhspfwlajht.supabase.co";
+const supabaseKey = "sb_publishable_e8qgwOwx6VkqlqHaXk-SoQ_91xlw3OL";
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Missing Supabase environment variables');
-}
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
